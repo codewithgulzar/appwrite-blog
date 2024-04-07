7fce9677618c56ee6e2d5fb69253caa69e1edec0
codewithgulzar <muhammad.gulzar8991@gmail.com>
2024-04-07 10:01:07 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+    console.log(import.meta.env.REACT_APP_APPWRITE_ENDPOINT);
+    return (
+        <>
+            <h1>A blog with appwrite</h1>
+        </>
+    );
+}
+
+export default App;
